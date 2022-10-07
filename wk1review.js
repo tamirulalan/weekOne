e870609688108ab5c6e3fdc04b1375ffee406c15
tamirulalan <tamirulalan@gmail.com>
2022-10-07 15:42:12 -0400

    this is the first push

--- wk1review.js
+++ wk1review.js
@@ -1,134 +1,183 @@
-////////////////////////////////////////////////////////////////
-// Conditionals:
-///////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////
+// // Conditionals:
+// ///////////////////////////////////////////////////////////////
 
-//1. Write a JavaScript display the larger of two integers
+// //1. Write a JavaScript display the larger of two integers
+// //name a function called largest that accect two parameters
+function largest(a, b) { 
+	if(a > b)
+		return a;
+	else if(a === b)
+		return 0;
+	else
+		return b;
+}
+console.log(largest(2,3));
 
-/* 2. Write an if/else statement for the following requirements:
+// /* 2. Write an if/else statement for the following requirements:
 
-If student gets 90 or higher: console log  A
-If students get 80 or above: console log B
-If students get 70 or above: console log C
-If students get 55 or above: console log D
-Any grade lower than 55 is F */
+// If student gets 90 or higher: console log  A
+// If students get 80 or above: console log B
+// If students get 70 or above: console log C
+// If students get 55 or above: console log D
+// Any grade lower than 55 is F */
+// let Grade =98;
+// if (Grade>=90){
+//   console.log("A")
+// }
+// else if (Grade>=80){
+//   console.log("B")
+// }
+// else if (Grade>=70){
+//   console.log("C")
+// }
+// else if (Grade>=55){
+//   console.log("D")
+// }
+// else if (Grade < 55){
+//   console.log("F")
+// }
 
-//3. Write a JavaScript program that displays "Good Morning" if time is between 5AM-11:59 | "Good Afternoon" if time is between 12 - 16 | otherwise "Hey there"
+// //3. Write a JavaScript program that displays "Good Morning" if time is between 5AM-11:59 | "Good Afternoon" if time is between 12 - 16 | otherwise "Hey there"
+// let timeIs = 3;
+// if (5 <= timeIs <12){
+//   console.log("Good Morning")
+// }
+// else if (12<= timeIs <=16){
+//   console.log("Good Afternoon")
+// }else{
+//   console.log("Hey there")
+// }
+// ////////////////////////////////////////////////////////////////
+// // Strings:
+// ///////////////////////////////////////////////////////////////
+// // Look up the following string methods!
 
-////////////////////////////////////////////////////////////////
-// Strings:
-///////////////////////////////////////////////////////////////
-// Look up the following string methods!
+// // Using charAt(), print the letter i in Sabrina
+// let letter ="Sabrina";
+// console.log(letter.charAt(4))
 
-// Using charAt(), print the letter i in Sabrina
+// // Define Unicode in a comment, and get the Unicode using charCodeAt() of the third character in Teddy
 
-// Define Unicode in a comment, and get the Unicode using charCodeAt() of the third character in Teddy
+// // Using fromCharCode() - make it readable for us :). You'll see!
 
-// Using fromCharCode() - make it readable for us :). You'll see!
+// // Take your first and last name and concat()
 
-// Take your first and last name and concat()
+// // Create a string and make it return true using startsWith()
 
-// Create a string and make it return true using startsWith()
+// // Now use any variable with endsWith() and return false
 
-// Now use any variable with endsWith() and return false
+// // Finish the following sentence. Use includes() and return true.
+// const ozgur = 'Once upon a time Ozgur looked up at the Moon, '
 
-// Finish the following sentence. Use includes() and return true.
-const ozgur = 'Once upon a time Ozgur looked up at the Moon, '
+// // Help! I'm looking for my cow! Using indexOf() help Josh find his cow. What's the index of cow?
+// const joshHadALittleLambOopsCow = "Josh lives in a farm. Josh was overwhelmed with coding. Josh is stressed out. Josh fed the chickens this morning. He watered his plants. He took his cow Betsy on a walk. Josh went home without Betsy. Josh went to bed that night, and realized he went home alone."
 
-// Help! I'm looking for my cow! Using indexOf() help Josh find his cow. What's the index of cow?
-const joshHadALittleLambOopsCow = "Josh lives in a farm. Josh was overwhelmed with coding. Josh is stressed out. Josh fed the chickens this morning. He watered his plants. He took his cow Betsy on a walk. Josh went home without Betsy. Josh went to bed that night, and realized he went home alone."
+// ////////////////////////////////////////////////////////////////
+// // Boolean:
+// ///////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////
-// Boolean:
-///////////////////////////////////////////////////////////////
+// // I just want my dreams to come true. 
+// // With the powers of logical and comparison operators, PRINT TRUE please!
+// // Replace the underscores.
 
-// I just want my dreams to come true. 
-// With the powers of logical and comparison operators, PRINT TRUE please!
-// Replace the underscores.
+// const a = 5;
+// const b = 10;
+// const c = 15;
+// const d = "Keke"
 
-const a = 5;
-const b = 10;
-const c = 15;
-const d = "Keke"
-
-// console.log(a _ b)
-// console.log(c _ b)
-// console.log(d _ d)
-// console.log(d _ a)
-// console.log(a _ 15)
-// console.log(a _ b _ c)
-// console.log(c _ b _ a _ d)
+// // console.log(a _ b)
+// // console.log(c _ b)
+// // console.log(d _ d)
+// // console.log(d _ a)
+// // console.log(a _ 15)
+// // console.log(a _ b _ c)
+// // console.log(c _ b _ a _ d)
 
 
-////////////////////////////////////////////////////////////////
-// Loops:
-///////////////////////////////////////////////////////////////
-// Ken has a headache today and he said the room has been spinning like crazy. Make Ken's room spin 10 times using FOR LOOP. Make him print a sentence.
+// ////////////////////////////////////////////////////////////////
+// // Loops:
+// ///////////////////////////////////////////////////////////////
+// // Ken has a headache today and he said the room has been spinning like crazy. Make Ken's room spin 10 times using FOR LOOP. Make him print a sentence.
 
-// Harry said that was so mean of Teo to do Ken dirty like that. Make Teo's vision spin 20 times FOR LOOP.
+// // Harry said that was so mean of Teo to do Ken dirty like that. Make Teo's vision spin 20 times FOR LOOP.
 
-// But wait! Lisa said, while Teo is spinning 20 times. Let's make him say "I'm sorry". Print I'm sorry 20 times using a WHILE loop.
+// // But wait! Lisa said, while Teo is spinning 20 times. Let's make him say "I'm sorry". Print I'm sorry 20 times using a WHILE loop.
 
-// For the next three, look for the syntax of FOR IN and FOR OF loop. Very easy to understand syntax, and very clean :) 
+// // For the next three, look for the syntax of FOR IN and FOR OF loop. Very easy to understand syntax, and very clean :) 
 
-// To The Left! To The Left! Everything I want in the console to the left. Print thing1, thing2, thing 3 using (FOR IN LOOP)
-const whateverQueenBeySaid = {
-    thing1: 'shoes',
-    thing2: 'clothes',
-    thing3: 'gaming console'
-  }
+// // To The Left! To The Left! Everything I want in the console to the left. Print thing1, thing2, thing 3 using (FOR IN LOOP)
+// const whateverQueenBeySaid = {
+//     thing1: 'shoes',
+//     thing2: 'clothes',
+//     thing3: 'gaming console'
+//   }
   
-  // Using (FOR IN LOOP), print the indexes of the array.
-  const lana = ['l', 'a', 'n', 'a']
+//   // Using (FOR IN LOOP), print the indexes of the array.
+//   const lana = ['l', 'a', 'n', 'a']
   
-  // USE (FOR OF LOOP)!
-  const tia = ['teo', 'the', 'syntax', 'of', 'this', 'is', 'cleaner']
+//   // USE (FOR OF LOOP)!
+//   const tia = ['teo', 'the', 'syntax', 'of', 'this', 'is', 'cleaner']
   
-  // Look into forEach() loop, it accepts a callback aka a function inside a function. 
-  // Swathi said don't forget each person. No person left behind! Use forEach() to iterate over the array perscholas. Inside the function, create a variable with the string "PS". Print their name in the console concatenated with the variable. 
-  const perscholas = ['Amira', 'Arely', 'Jonathan']
+//   // Look into forEach() loop, it accepts a callback aka a function inside a function. 
+//   // Swathi said don't forget each person. No person left behind! Use forEach() to iterate over the array perscholas. Inside the function, create a variable with the string "PS". Print their name in the console concatenated with the variable. 
+//   const perscholas = ['Amira', 'Arely', 'Jonathan']
 
-////////////////////////////////////////////////////////////////
-// Arrays:
-///////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////
+// // Arrays:
+// ///////////////////////////////////////////////////////////////
 
-// Look up the following: Make a note of what each one of these methods do
-// push()
-// pop()
-// unshift()
-// shift()
-// concat()
-// splice()
-// slice()
-// sort()
-// includes()
-// indexOf()
-// length
+// // Look up the following: Make a note of what each one of these methods do
+// // push()
+// // pop()
+// // unshift()
+// // shift()
+// // concat()
+// // splice()
+// // slice()
+// // sort()
+// // includes()
+// // indexOf()
+// // length
 
-/* You've been tasked to do this week's grocery shopping. As you arrive at Times Supermarket, Raphael pings you to get a bottle of Hendricks gin. Add this to the existing shoppingList and console.log it.*/
-var shoppingList = [
+// /* You've been tasked to do this week's grocery shopping. As you arrive at Times Supermarket, Raphael pings you to get a bottle of Hendricks gin. Add this to the existing shoppingList and console.log it.*/
+// var shoppingList = [
 
-  "cool ranch doritos",
+//   "cool ranch doritos",
 
-  "kings hawaiian sweet bread",
+//   "kings hawaiian sweet bread",
 
-  "peanut butter oreos",
+//   "peanut butter oreos",
 
-  "fruit loops cereal"
+//   "fruit loops cereal"
 
-];
+// ];
 
-//3.  Use the force, or in this case the reverse method to help Yoda make some sense with his motivational talk to the young Jedi interns.*/
+// //3.  Use the force, or in this case the reverse method to help Yoda make some sense with his motivational talk to the young Jedi interns.*/
 
-var yoda = ["try", "no", "is", "there", "not", "do", "or", "do"];
+// var yoda = ["try", "no", "is", "there", "not", "do", "or", "do"];
 
 //4. People been lining up for hours to get the newest iphone release. Help manage the unruly crowd of privileged customers by serving them one at a time and assiging it to a variable named `nowServing`. Console.log this new variable as well as the waitList.*/
 
 var waitList = [ "Chance the Rapper", "Khalid", "Tay-Tay", "Barry Manilow", "Piko Taro" ];
+var nowServing =""
+while(waitList.length >0){
+ nowServing = waitList.shift()
+ console.log(nowServing)
+}
+
 
 //5. Help Nike sell more overpriced sneakers by changing their slogan from an array into a string. Convert the shoe array and assign it to a variable named `shoeString`. Console.log this new variable.
 
 var shoe = ["just", "do", "it"];
+var shoeString = ""
+for (let i=0; i<shoe.length; i++){
+  shoeString +=shoe[i] +" " 
+}
+console.log(shoeString )
 
+ var s = shoe.join(" ")
+ console.log(s)
 // ===== SPREAD OPERATOR ===== 
 
 // Create a variable and assign a value of a copied array using the spread operator
